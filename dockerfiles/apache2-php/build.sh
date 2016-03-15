02812aa822d6691a985e6e2215e32f06e6da4fe5
yonh <azssjli@163.com>
2016-03-15 17:04:34 +0800

    update image build script

--- dockerfiles/apache2-php/build.sh
+++ dockerfiles/apache2-php/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker build -t tinystime/php-apache2 .
+
+if [ "$1" = "dev" ]; then
+	cp Dockerfile1 Dockerfile
+	docker build -t tinystime/php-apache2 .
+else
+	cp Dockerfile2 Dockerfile
+	docker build -t tinystime/php-apache2 .
+fi
