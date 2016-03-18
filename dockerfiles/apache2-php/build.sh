1e0e43163181eeafa25583017b3d38d0a2e812fa
yonh <azssjli@163.com>
2016-03-18 13:22:38 +0800

    update php dockerfile

--- dockerfiles/apache2-php/build.sh
+++ dockerfiles/apache2-php/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$1" = "dev" ]; then
-	sed -i 's/#RUN userdel www-data/RUN userdel www-data/g' Dockerfile
+	sed -i 's/# RUN userdel www-data/RUN userdel www-data/g' Dockerfile
 	docker build -t tinystime/php-apache2:dev .
 else
-	sed -i 's/RUN userdel www-data/#RUN userdel www-data/g' Dockerfile
-	docker build -t tinystime/php .
+	sed -i 's/^RUN userdel www-data/# RUN userdel www-data/g' Dockerfile
+	docker build -t tinystime/php-apache2 .
 fi
