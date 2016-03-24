54a8aa84f60e619d178b3052e9e817216948bfc9
yonh <azssjli@163.com>
2016-03-24 23:35:03 +0800

    fix stdout out error

--- tiny_update_hook.sh
+++ tiny_update_hook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nohup sudo -u www-data /opt/tiny_dep/project_update_listen > /var/log/tiny_update_hook.log 2 > /var/log/tiny_update_hook.err &
+nohup sudo -u www-data /opt/tiny_dep/project_update_listen 2 > /var/log/tiny_update_hook.err 1 > /var/log/tiny_update_hook.log &
 
 #start-stop-daemon --start --user www-data --oknodo --pidfile /var/run/tiny.pid --exec /opt/tiny_dep/project_update_listen > /dev/null
