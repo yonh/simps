6c8fed02a2153969bf439c2197587c7904c2b336
yonh <azssjli@163.com>
2016-03-25 11:28:25 +0800

    如果安装了docker就不重新安装

--- docker-install.sh
+++ docker-install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# 如果安装了docker就退出
+if [ -x "/usr/bin/docker" ]; then
+	exit 0
+end
+
 apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >  /etc/apt/sources.list.d/docker.list
