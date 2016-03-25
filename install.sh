eccb6419b54d29d64fc1aa28f7d3630ab8d2fc80
yonh <azssjli@163.com>
2016-03-25 14:51:34 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -28,8 +28,8 @@ cp nginx.conf.tpl $app_dir/
 # 设置www-data的ssh-key
 mkdir -p /var/www && chown www-data.www-data /var/www -R
 sudo -u www-data ssh-keygen -t rsa
-ssh-keyscan -p 2222 -t ecdsa,rsa 45.55.4.132 >> /var/www/.ssh/known_hosts
-chown www-data.www-data /var/www -R
+#可使用下面命令添加known_hosts
+#ssh-keyscan -p 端口 -t ecdsa,rsa 你的ip >> /var/www/.ssh/known_hosts
 
 # 设置更新程序开机启动
 if [ ! -f "/etc/init.d/tiny_update_hook" ]; then
@@ -39,4 +39,7 @@ else
 	cp tiny_update_hook.sh /etc/init.d/tiny_update_hook
 fi
 
+echo "================提示===================="
 echo "请修改文件$app_dir/db/server_ip内容更改为您服务器ip"
+echo "请修改您的www-data账户的known_hosts文件使得hook服务可以顺利拉取代码"
+echo "或可调用命令增加(注意权限),ssh-keyscan -p 端口 -t ecdsa,rsa 服务器ip >> /var/www/.ssh/known_hosts"
