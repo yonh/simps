9f36d08a70d31e1e2e4c6c74925f8c2094588c78
yonh <azssjli@163.com>
2016-03-25 11:29:41 +0800

    更新www-data用户ssh-key配置,更新hook脚本启动时停止旧进程

--- install.sh
+++ install.sh
@@ -25,8 +25,11 @@ cp project_update_listen.rb $app_dir/project_update_listen && chmod +x $app_dir/
 cp functions.rb $app_dir/
 cp nginx.conf.tpl $app_dir/
 
-echo "请修改文件$app_dir/db/server_ip内容更改为您服务器ip"
-
+# 设置www-data的ssh-key
+mkdir -p /var/www && chown www-data.www-data /var/www -R
+sudo -u www-data ssh-keygen -t rsa
+ssh-keyscan -p 2222 -t ecdsa,rsa 45.55.4.132 >> /var/www/.ssh/known_hosts
+chown www-data.www-data /var/www -R
 
 # 设置更新程序开机启动
 if [ ! -f "/etc/init.d/tiny_update_hook" ]; then
@@ -35,3 +38,5 @@ if [ ! -f "/etc/init.d/tiny_update_hook" ]; then
 else
 	cp tiny_update_hook.sh /etc/init.d/tiny_update_hook
 fi
+
+echo "请修改文件$app_dir/db/server_ip内容更改为您服务器ip"
--- tiny_update_hook.sh
+++ tiny_update_hook.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 干掉运行中的程序
+ps -aux |grep project_update_listen|grep -v grep |awk '{print $2}' |xargs kill -9
+
 nohup sudo -u www-data /opt/tiny_dep/project_update_listen 2 > /var/log/tiny_update_hook.err 1 > /var/log/tiny_update_hook.log &
 
 #start-stop-daemon --start --user www-data --oknodo --pidfile /var/run/tiny.pid --exec /opt/tiny_dep/project_update_listen > /dev/null
