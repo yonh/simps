1c7b187bd8d9e7e8506e6a7873d15733b0d0e8a3
yonh <azssjli@163.com>
2016-04-11 14:50:18 +0800

    add image apache2-laravel

---
+++ dockerfiles/apache2-laravel/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+chown www-data.www-data /www -R
+
+LOCKFILE=/run/apache2/apache2.pid
+[ -f $LOCKFILE ] && exit 0
+trap "{ rm -f $LOCKFILE ; exit 255; }" EXIT
+/usr/sbin/apache2ctl -DFOREGROUND
+
+#exit 0
