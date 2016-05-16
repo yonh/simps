b8d3aa4ebe6efa37f37e9dcb761709fbd472a685
yonh <azssjli@163.com>
2016-05-16 11:33:49 +0800

    Update docker-install.sh
    修正脚本if结束错误

--- docker-install.sh
+++ docker-install.sh
@@ -2,7 +2,7 @@
 # 如果安装了docker就退出
 if [ -x "/usr/bin/docker" ]; then
 	exit 0
-end
+fi
 
 apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
